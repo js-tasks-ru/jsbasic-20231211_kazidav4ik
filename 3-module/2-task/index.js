5fba48656275da0f0f19929a5c94e30dd3cf5424
David Kazimirovskiy <dkazimirovskiy@gmail.com>
2024-01-16 15:06:26 +0000

    решил 2ю задачу

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,10 @@
 function filterRange(arr, a, b) {
   // ваш код...
+  let filter = [];
+  for (let i = 0; i <= arr.length - 1; i++) {
+    if (arr[i] >= a && arr[i] <= b) {
+      filter.push(arr[i]);
+    }
+  }
+  return filter;
 }
