438f940e9d37075f9062bb353971004725379f8b
David Kazimirovskiy <dkazimirovskiy@gmail.com>
2024-01-18 16:15:48 +0000

    решил 3ю задачу

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,32 @@
 function highlight(table) {
-  // ваш код...
+  // ваш код...let rows = table.getElementsByTagName("tr");
+
+  for (let i = 1; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    let status = row.cells[3].getAttribute("data-available");
+
+    if (status === "true") {
+      row.classList.add("available");
+    } else if (status === "false") {
+      row.classList.add("unavailable");
+    }
+
+    if (!row.cells[3].hasAttribute("data-available")) {
+      row.hidden = true;
+    }
+
+    let sex = row.cells[2].textContent;
+
+    if (sex === "m") {
+      row.classList.add("male");
+    } else if (sex === "f") {
+      row.classList.add("female");
+    }
+
+    let age = Number(row.cells[1].textContent);
+
+    if (age < 18) {
+      row.style.textDecoration = "line-through";
+    }
+  }
 }
