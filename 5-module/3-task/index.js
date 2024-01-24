ff715a2bf7467f09a9595e92f5f04023fe66338f
David Kazimirovskiy <dkazimirovskiy@gmail.com>
2024-01-24 15:38:28 +0000

    caousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -4,12 +4,12 @@ function initCarousel() {
   const carouselArrowLeft = document.querySelector(".carousel__arrow_left");
   const carouselArrowRight = document.querySelector(".carousel__arrow_right");
   const slideWidth = carouselInner.offsetWidth;
-  const slideCount = 4;
+  const slideNumber = 4;
   let currentSlide = 0;
   function updateArrows() {
     carouselArrowLeft.style.display = currentSlide === 0 ? "none" : "";
     carouselArrowRight.style.display =
-      currentSlide === slideCount - 1 ? "none" : "";
+      currentSlide === slideNumber - 1 ? "none" : "";
   }
   function moveCarousel() {
     carouselInner.style.transform = `translateX(-${
